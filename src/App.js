e347147e07e5b79da7e7860c37d6187c5b94d47c
menna afifi <mm1078074@gmail.com>
2023-04-04 02:34:30 +0200

    firstchapter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./shared/Header";
+import Footer from "./shared/Footer";
+import './style/App.css';
+import { Outlet } from "react-router-dom";
+import { Routes,Route } from 'react-router-dom';
+import Create   from "./pages/chapters/component/Create";
+import Home from './pages/chapters/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Header/>
+   <Outlet/>
+   <Footer/>
+
+   <Routes>
+      
+      <Route path='/' element={<Home/>}></Route>
+      
+      <Route path='/create' element={<Create/>}></Route>
+      
+    
+    </Routes>
+   
+   
+   
+   </>
   );
-}
+};
 
 export default App;
